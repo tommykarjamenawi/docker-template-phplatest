a74ff78c803d24458d797d165584661dd3358c52
tommykarjamenawi <tommy_12345@live.nl>
2021-11-10 11:31:22 +0100

    code cleanup

--- app/public/demo.php
+++ app/public/demo.php
@@ -5,9 +5,7 @@
     error_reporting(E_ALL);
 
     // Startup code goes here
-    //$birth_date = '1991-12-10';
     $birth_date = $_GET["age"];
-    //$actualAge = strtotime($birthDate);
  
     function getAgeFromString($birth_date) { // calculate age from a string
         $birth_date = explode("-", $birth_date);
@@ -16,12 +14,11 @@
             : (date("Y") - $birth_date[0]));
         return $age;
     }
-    $actualAge = getAgeFromString($birth_date);
+    $actualAge = getAgeFromString($birth_date); // converte date to age
     
     if (isset($_GET["firstname"], $_GET["age"])){
         echo $_GET["firstname"];
         echo "</br>";
         echo $actualAge;
-        //echo $age= date("Y") - date("Y", strtotime($birth_date)); // convert string to date
       }
 ?>
