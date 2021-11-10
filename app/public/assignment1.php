f31f65fb9a101da09dc1913801858c32259e2223
tommykarjamenawi <tommy_12345@live.nl>
2021-11-10 07:34:17 +0100

    some updates

--- app/public/assignment1.php
+++ app/public/assignment1.php
@@ -1,9 +1,13 @@
 <?php
-    $age;
-    $age += 75;
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    // Startup code goes here
+    $age = readline("Enter your age: ");
+    /*$age += 75;
     $age -= 25;
     $age *= 5;
-    $age /= 50;
+    $age /= 50;*/
     if ($age > 0 && $age < 5) {
         echo "Below average";
     } 
